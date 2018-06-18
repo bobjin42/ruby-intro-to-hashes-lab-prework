163005b6c9a1f8bbbcaa41cce4979536fcc9ef9d
Yicheng Jin <bobjin42@gmail.com>
2018-06-18 16:58:04 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,9 @@ end
 def monopoly
 	monopoly = {:railroads => {
 	  :pieces => 4,
-	  :names => {},
+	  :names => {
+	    
+	  },
 	  :rent_in_dollars => {
 	    :one_piece_owned => 25,
 	    :two_pieces_owned => 50,
