864d68c2bc091c9b315b871b116035c9fb2e565c
Yicheng Jin <bobjin42@gmail.com>
2018-06-18 17:00:27 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,12 +11,14 @@ def monopoly
 	  :pieces => 4,
 	  :names => {
 	    :reading_railroad => {
-	      mortgage_value: '$100',
+	      mortgage_value: '$100'
 	    },
 	    :pennsylvania_railroad => {
-	      mortgage_value: 
+	      mortgage_value: '$200'
 	    },
-	    :b_and_o_railroad => {},
+	    :b_and_o_railroad => {
+	      
+	    },
 	    :shortline => {}
 	  },
 	  :rent_in_dollars => {
